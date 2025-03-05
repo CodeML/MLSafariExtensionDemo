f882da10d9cea9e211c6a78d8c4590487704d52c
魏明磊 <weiminglei1990@163.com>
2025-03-05 11:09:47 +0800

    Initial Commit

---
+++ MLSafariExtensionDemo/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  MLSafariExtensionDemo
+//
+//  Created by 魏明磊 on 2025/3/5.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
